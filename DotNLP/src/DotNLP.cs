65d6b8bfeaa4a61490a05fbb298512bcf257aac3
eric-sclafani <eric.sclafani321@gmail.com>
2025-05-17 16:04:25 -0400

    initial commit

---
+++ DotNLP/src/DotNLP.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace DotNLP;
+
+public class DotNLP
+{
+	private const string SPACE = " ";
+
+	public IEnumerable<char> CharTokenize(string text)
+	{
+		return text.ToCharArray();
+	}
+
+	public IEnumerable<string> WordTokenize(string text)
+	{
+		IList<string> tokens = [];
+
+		var currentWord = new StringBuilder("");
+
+		for (var i = 0; i < text.Length; i++)
+		{
+			var cString = text[i].ToString();
+			if (char.IsPunctuation(text[i]))
+			{
+				if (currentWord.ToString() != "")
+				{
+					tokens.Add(currentWord.ToString());
+					currentWord.Clear();
+				}
+				tokens.Add(cString);
+			}
+			
+			else if (cString == SPACE || i == text.Length - 1)
+			{
+				if (currentWord.ToString() != "")
+				{
+					tokens.Add(currentWord.ToString());
+					currentWord.Clear();
+				}
+			}
+			else
+			{
+				currentWord.Append(cString);
+			}
+		}
+
+		return tokens;
+	}
+}
---
+++ TestingConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestingConsoleApp;
+
+using DotNLP;
+
+internal class Program
+{
+	public static void Main()
+	{
+		var nlp = new DotNLP();
+		var test1 = "This is my sentence. It's a good and amazing sentence.";
+		var test2 = "This is my sentence with consecutive punctuation!!. It's a good and amazing sentence.";
+		var test3 = "Let's go to the park today!!! Wouldn't that be nice? I would think so...";
+		var tokens = nlp.WordTokenize(test3);
+		foreach (var tok in tokens)
+		{
+			Console.WriteLine(tok);
+		}
+	}
+}
