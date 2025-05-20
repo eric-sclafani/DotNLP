d2bec7a4d039dd1dae37045bddebe8e730424fc2
eric-sclafani <eric.sclafani321@gmail.com>
2025-05-20 18:08:50 -0400

    added contraction, possessive, punctuation handling

--- DotNLP/src/DotNLP.cs
+++ DotNLP/src/DotNLP.cs
@@ -4,46 +4,86 @@ namespace DotNLP;
 
 public class DotNLP
 {
-	private const string SPACE = " ";
-
-	public IEnumerable<char> CharTokenize(string text)
-	{
-		return text.ToCharArray();
-	}
-
 	public IEnumerable<string> WordTokenize(string text)
 	{
-		IList<string> tokens = [];
+		IList<string> textTokens = text.Split(" ");
+		IList<string> resultTokens = [];
 
-		var currentWord = new StringBuilder("");
-
-		for (var i = 0; i < text.Length; i++)
+		foreach (var token in textTokens)
 		{
-			var cString = text[i].ToString();
-			if (char.IsPunctuation(text[i]))
+			if (IsContractionOrPossessive(token) && !IsSpecialException((token)))
 			{
-				if (currentWord.ToString() != "")
-				{
-					tokens.Add(currentWord.ToString());
-					currentWord.Clear();
-				}
-				tokens.Add(cString);
+				HandleContractionsPossessives(resultTokens, token);
 			}
-			
-			else if (cString == SPACE || i == text.Length - 1)
+
+			else if (token.Any(char.IsPunctuation))
 			{
-				if (currentWord.ToString() != "")
-				{
-					tokens.Add(currentWord.ToString());
-					currentWord.Clear();
-				}
+				HandlePunctuation(resultTokens, token);
 			}
+
 			else
 			{
-				currentWord.Append(cString);
+				resultTokens.Add(token);
 			}
 		}
 
-		return tokens;
+		return resultTokens;
+	}
+
+	private static void HandlePunctuation(IList<string> tokens, string token)
+	{
+		var puncIndex = token.IndexOf(token.FirstOrDefault(char.IsPunctuation));
+		tokens.Add(token[..puncIndex]);
+
+		if (token[puncIndex..] == "...")
+		{
+			tokens.Add("...");
+		}
+		else
+		{
+			foreach (var c in token[puncIndex..])
+			{
+				tokens.Add(c.ToString());
+			}
+		}
+	}
+
+	private static void HandleContractionsPossessives(IList<string> tokens, string token)
+	{
+		if (token.Count(c => c.ToString() == "'") == 1)
+		{
+			if (token.EndsWith("n't"))
+			{
+				var i = token.IndexOf("n't", StringComparison.Ordinal);
+				tokens.Add(token[..i]);
+				tokens.Add("n't");
+			}
+			else
+			{
+				var i = token.IndexOf('\'');
+				tokens.Add(token[..i]);
+				tokens.Add(token[i..]);
+			}
+		}
+	}
+
+	private static bool IsContractionOrPossessive(string token)
+	{
+		token = token.ToLower();
+		return token.EndsWith("'s") ||
+		       token.EndsWith("'re") ||
+		       token.EndsWith("'ve") ||
+		       token.EndsWith("'ll") ||
+		       token.EndsWith("'d") ||
+		       token.EndsWith("n't") ||
+		       token.EndsWith("'m");
+	}
+
+	private static bool IsSpecialException(string token)
+	{
+		token = token.ToLower();
+		return token == "ain't" ||
+		       token == "y'all" ||
+		       token == "ma'am";
 	}
 }
--- TestingConsoleApp/Program.cs
+++ TestingConsoleApp/Program.cs
@@ -9,7 +9,8 @@ internal class Program
 		var nlp = new DotNLP();
 		var test1 = "This is my sentence. It's a good and amazing sentence.";
 		var test2 = "This is my sentence with consecutive punctuation!!. It's a good and amazing sentence.";
-		var test3 = "Let's go to the park today!!! Wouldn't that be nice? I would think so...";
+		var test3 =
+			"Let's go to the park today!!! Wouldn't that be nice? I would think so... But I ain't going! I think Joseph's dog will go though.";
 		var tokens = nlp.WordTokenize(test3);
 		foreach (var tok in tokens)
 		{
